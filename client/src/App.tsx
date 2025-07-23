d8f7aabe059dee11a4f0f6ee1529ee1a04951563
Thales Raymond <32554150+thalesraymond@users.noreply.github.com>
2025-07-23 20:43:44 -0300

    basic routing (#6)

--- client/src/App.tsx
+++ client/src/App.tsx
@@ -1,5 +1,21 @@
+import { createBrowserRouter, RouterProvider } from "react-router-dom";
+
+const router = createBrowserRouter([
+  {
+    path: "/",
+    element: <h1>home</h1>,
+  },
+  {
+    path: "/about",
+    element: (
+      <div>
+        <h1>about</h1>
+      </div>
+    ),
+  },
+]);
 const App = () => {
-  return <h1>Jobify App</h1>;
+  return <RouterProvider router={router} />;
 };
 
 export default App;
