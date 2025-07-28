11e348a6b96ce7bbc28c017654415f90363485bc
Thales Raymond <32554150+thalesraymond@users.noreply.github.com>
2025-07-28 19:27:44 -0300

    simple links (#9)

--- client/src/pages/Login.tsx
+++ client/src/pages/Login.tsx
@@ -1,9 +1,12 @@
+import { Link } from "react-router-dom";
+
 const Login = () => {
-    return (
-        <div>
-            <h1>Login</h1>
-        </div>
-    )
-}
+  return (
+    <div>
+      <h1>Login Page</h1>
+      <Link to="/register">Register Page</Link>
+    </div>
+  );
+};
 
 export default Login;
--- client/src/pages/Register.tsx
+++ client/src/pages/Register.tsx
@@ -1,9 +1,12 @@
+import { Link } from "react-router-dom";
+
 const Register = () => {
-    return (
-      <div>
-        <h1>Register</h1>
-      </div>
-    );
-}
+  return (
+    <div>
+      <h1>Register Page</h1>
+      <Link to="/login">Login Page</Link>
+    </div>
+  );
+};
 
 export default Register;
