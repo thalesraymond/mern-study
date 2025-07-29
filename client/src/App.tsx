a591e415ba7b87c45508d26b142a328857443e36
Thales Raymond <32554150+thalesraymond@users.noreply.github.com>
2025-07-29 18:25:15 -0300

    feat: nested routes example

--- client/src/App.tsx
+++ client/src/App.tsx
@@ -1,23 +1,29 @@
 import { createBrowserRouter, RouterProvider } from "react-router-dom";
-import { DashboardLayout, HomeLayout, Login, Register } from "./pages";
+import { DashboardLayout, HomeLayout, Landing, Login, Register } from "./pages";
 
 const router = createBrowserRouter([
   {
     path: "/",
-    element: < HomeLayout />,
+    element: <HomeLayout />,
+    children: [
+      {
+        index: true,
+        element: <Landing />,
+      },
+      {
+        path: "register",
+        element: <Register />,
+      },
+      {
+        path: "login",
+        element: <Login />,
+      },
+      {
+        path: "dashboard",
+        element: <DashboardLayout />,
+      },
+    ],
   },
-  {
-    path: "/register",
-    element: < Register />,
-  },
-  {
-    path: "/login",
-    element: < Login />,
-  },
-  {
-    path: "/dashboard",
-    element: < DashboardLayout />,
-  }
 ]);
 const App = () => {
   return <RouterProvider router={router} />;
