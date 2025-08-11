3825a26655969706106d6f8d3aa32f74e9b47e0b
Thales Raymond <32554150+thalesraymond@users.noreply.github.com>
2025-08-11 18:16:55 -0300

    feat:404 error handling page

--- client/src/pages/Error.tsx
+++ client/src/pages/Error.tsx
@@ -1,12 +1,27 @@
 import { Link, useRouteError } from "react-router-dom";
+import Wrapper from "../assets/wrappers/ErrorPage";
+import img from "../assets/images/not-found.svg";
+import {DashboardLayout} from "./index.ts";
 
 const Error = () => {
     const error = useRouteError();
     console.log(error);
+
+    const httpError = error as {status: number, message: string};
+
+    if (httpError && httpError.status === 404) {
+        return <Wrapper>
+            <div>
+                <img src={img} alt="not found" />
+                <h3>Ohh! page not found</h3>
+                <p>We can't seem to find the page you're looking for</p>
+                <Link to="/dashboard">back home</Link>
+            </div>
+        </Wrapper>
+    }
     return (
         <div>
-            <h1>There was an error...</h1>
-            <Link to="/">back home</Link>
+            <h3>Something went wrong</h3>
         </div>
     );
 }
