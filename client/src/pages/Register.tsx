ad1f0203a68a73c0253e3bedaf57e4e871ff1e92
Thales Raymond <32554150+thalesraymond@users.noreply.github.com>
2025-08-11 18:44:37 -0300

    feat: register front end and formrow component (#15)

--- client/src/pages/Register.tsx
+++ client/src/pages/Register.tsx
@@ -1,11 +1,40 @@
 import { Link } from "react-router-dom";
+import Wrapper from "../assets/wrappers/RegisterAndLoginPage";
+import { FormRow, Logo } from "../components";
 
 const Register = () => {
   return (
-    <div>
-      <h1>Register Page</h1>
-      <Link to="/login">Login Page</Link>
-    </div>
+    <Wrapper>
+      <form className="form">
+        <Logo />
+        <h4>Register</h4>
+
+        <FormRow type="text" name="name" defaultValue="john" />
+
+        <FormRow
+          type="text"
+          name="lastName"
+          labelText="last name"
+          defaultValue="smith"
+        />
+
+        <FormRow type="text" name="location" defaultValue="earth" />
+
+        <FormRow type="email" name="email" defaultValue="john@gmail.com" />
+
+        <FormRow type="password" name="password" defaultValue="secret123" />
+
+        <button type="submit" className="btn btn-block">
+          submit
+        </button>
+        <p>
+          Already a member?
+          <Link to="/login" className="member-btn">
+            Login
+          </Link>
+        </p>
+      </form>
+    </Wrapper>
   );
 };
 
