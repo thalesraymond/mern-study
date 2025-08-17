29b25b0a990e4da0e73cda4c1690d818a206c46f
Thales Raymond <32554150+thalesraymond@users.noreply.github.com>
2025-08-17 13:04:10 -0300

    icons

--- client/src/components/Navbar.tsx
+++ client/src/components/Navbar.tsx
@@ -1,6 +1,12 @@
 import Wrapper from "../assets/wrappers/Navbar";
+import { FaHome } from "react-icons/fa";
 const Navbar = () => {
-  return <Wrapper>Navbar</Wrapper>;
+  return (
+    <Wrapper>
+      <h2>navbar</h2>
+      <FaHome />
+    </Wrapper>
+  );
 };
 
 export default Navbar;
