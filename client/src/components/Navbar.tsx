696ba8a25f083a9245f01dd45acfb13683e18c31
Thales Raymond <32554150+thalesraymond@users.noreply.github.com>
2025-08-17 13:15:05 -0300

    feat:navbar toggle (#21)

--- client/src/components/Navbar.tsx
+++ client/src/components/Navbar.tsx
@@ -1,10 +1,22 @@
 import Wrapper from "../assets/wrappers/Navbar";
-import { FaHome } from "react-icons/fa";
+import { FaAlignLeft } from "react-icons/fa";
+import Logo from "./Logo";
+import { useDashboardContext } from "../pages/DashboardContext";
 const Navbar = () => {
+  const { toggleSidebar } = useDashboardContext();
   return (
     <Wrapper>
-      <h2>navbar</h2>
-      <FaHome />
+      <div className="nav-center">
+        <button type="button" className="toggle-btn"
+        onClick={toggleSidebar}>
+          <FaAlignLeft />
+        </button>
+        <div>
+          <Logo />
+          <h4 className="logo-text">dashboard</h4>
+        </div>
+        <div className="btn-container">toggle/logout</div>
+      </div>
     </Wrapper>
   );
 };
