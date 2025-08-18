1df6eb148a5d2d83ff506cc6ba778a44cff21364
Thales Raymond <32554150+thalesraymond@users.noreply.github.com>
2025-08-18 18:35:10 -0300

    feat:small side bar navigation and open/close

--- client/src/components/SmallSidebar.tsx
+++ client/src/components/SmallSidebar.tsx
@@ -1,11 +1,56 @@
+import { FaTimes } from "react-icons/fa";
 import Wrapper from "../assets/wrappers/SmallSidebar";
 import { useDashboardContext } from "../pages/DashboardContext";
+import Logo from "./Logo";
+import links from "../utils/Links";
+import type { JSX } from "react";
+import { NavLink } from "react-router-dom";
 
 const SmallSidebar = () => {
   const data = useDashboardContext();
 
   console.log(data);
-  return <Wrapper>Small Sidebar</Wrapper>;
+  return (
+    <Wrapper>
+      <div
+        className={
+          data.showSidebar
+            ? "sidebar-container show-sidebar"
+            : "sidebar-container"
+        }
+      >
+        <div className="content">
+          <button
+            type="button"
+            className="close-btn"
+            onClick={() => data.toggleSidebar()}
+          >
+            <FaTimes />
+          </button>
+          <header>
+            <Logo />
+          </header>
+          <div className="nav-links">
+            {links.map(
+              (link: { text: string; path: string; icon: JSX.Element }) => {
+                return (
+                  <NavLink
+                    to={link.path}
+                    key={link.text}
+                    className={"nav-link"}
+                    onClick={() => data.toggleSidebar()}
+                  >
+                    <span className="icon">{link.icon}</span>
+                    <span className="text">{link.text}</span>
+                  </NavLink>
+                );
+              }
+            )}
+          </div>
+        </div>
+      </div>
+    </Wrapper>
+  );
 };
 
 export default SmallSidebar;
