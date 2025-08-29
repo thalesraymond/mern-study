f20a0708d7ae9c413d474323c5865e2e6fcb08ec
Thales Raymond <32554150+thalesraymond@users.noreply.github.com>
2025-08-28 21:47:09 -0300

    feat:(setup morgan, dovenv and json middleware)

--- src/server.ts
+++ src/server.ts
@@ -1,12 +1,27 @@
 import express from "express";
 import test from "./nested/testfile.js";
+import morgan from "morgan";
+import * as dotenv from "dotenv";
 
 const app = express();
 
+dotenv.config();
+
+if (process.env.NODE_ENV === "development") {
+  app.use(morgan("dev"));
+}
+
+app.use(express.json());
+
 app.get("/", (req, res) => {
-    res.send("Hello World!!" + test);
+    res.send({ msg: "Hello World!!" + test});
 });
 
-app.listen(5100, () => {
+app.listen(process.env.PORT ?? 5100, () => {
     console.log("server running....");
 });
+
+app.post("/api/v1/test", (req, res) => {
+    const { name } = req.body;
+    res.json({ msg: `hello ${name}!` });
+}); 
