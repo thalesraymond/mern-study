ad3f31c66f4c368c9fe88efdd86d225f94a5ffc1
Thales Raymond <32554150+thalesraymond@users.noreply.github.com>
2025-09-07 15:16:58 -0300

    reorganize and add put (#31)

---
+++ src/JobRoutes.ts
@@ -0,0 +1,84 @@
+
+// GET Jobs
+import {StatusCodes} from "http-status-codes";
+import nanoid from "nanoid";
+import express, {Request, Response} from "express";
+
+const jobRoutes = express.Router();
+
+const jobs = [
+    {
+        id: nanoid(),
+        company: "Google",
+        position: "Software Engineer",
+    },
+    {
+        id: nanoid(),
+        company: "Microsoft",
+        position: "Software Engineer",
+    },
+    {
+        id: nanoid(),
+        company: "Amazon",
+        position: "Software Engineer",
+    }
+]
+
+jobRoutes.get("/api/v1/jobs", (req, res) => {
+    res.status(StatusCodes.OK).json({jobs});
+});
+
+// POST Create Job
+jobRoutes.post("/api/v1/jobs", (req, res) => {
+
+    const {company, position} = req.body;
+
+    if (!company || !position) {
+        return res.status(StatusCodes.BAD_REQUEST).json({msg: "Invalid request"});
+    }
+
+    const id = nanoid();
+    const job = {id, company, position};
+
+    jobs.push(job);
+
+    res.status(StatusCodes.CREATED).json({job});
+});
+
+jobRoutes.get("/api/v1/jobs/:id", (req: Request, res: Response) => {
+    const {id} = req.params;
+
+    if (!id) {
+        return res.status(StatusCodes.BAD_REQUEST).json({msg: "Invalid request"});
+    }
+
+    const foundJob = jobs.find(job => job.id === id);
+
+    if(foundJob) {
+        return res.status(StatusCodes.OK).json(jobs.find(job => job.id === id));
+    }
+
+    return res.status(StatusCodes.NOT_FOUND).json({msg: "Job not found"});
+});
+
+jobRoutes.put("/api/v1/jobs/:id", (req: Request, res: Response) => {
+    const {id} = req.params
+
+    const {company, position} = req.body;
+    if (!id || !company || !position) {
+        return res.status(StatusCodes.BAD_REQUEST).json({msg: "Invalid request"});
+    }
+
+    const jobToEdit = jobs.find(job => job.id === id);
+
+    if (!jobToEdit) {
+        return res.status(StatusCodes.NOT_FOUND).json({msg: "Job not found"});
+    }
+
+    jobToEdit.company = company;
+    jobToEdit.position = position;
+
+    return res.status(StatusCodes.OK).json(jobToEdit);
+})
+
+export default jobRoutes;
--- src/server.ts
+++ src/server.ts
@@ -1,26 +1,9 @@
 import express from "express";
+import { Request, Response } from 'express';
 import morgan from "morgan";
 import * as dotenv from "dotenv";
-import nanoid from "nanoid";
-import {StatusCodes} from "http-status-codes";
+import jobRoutes from "./JobRoutes";
 
-const jobs = [
-    {
-        id: nanoid(),
-        company: "Google",
-        position: "Software Engineer",
-    },
-    {
-        id: nanoid(),
-        company: "Microsoft",
-        position: "Software Engineer",
-    },
-    {
-        id: nanoid(),
-        company: "Amazon",
-        position: "Software Engineer",
-    }
-]
 
 const app = express();
 
@@ -32,7 +15,9 @@ if (process.env.NODE_ENV === "development") {
 
 app.use(express.json());
 
-app.get("/", (req, res) => {
+app.use(jobRoutes);
+
+app.get("/", (req: Request, res: Response) => {
     res.send({msg: "Hello World!!"});
 });
 
@@ -40,46 +25,9 @@ app.listen(process.env.PORT ?? 5100, () => {
     console.log("server running....");
 });
 
-app.post("/api/v1/test", (req, res) => {
+app.post("/api/v1/test", (req: Request, res: Response) => {
     const {name} = req.body;
     res.json({msg: `hello ${name}!`});
 });
 
-
-// GET Jobs
-app.get("/api/v1/jobs", (req, res) => {
-    res.status(StatusCodes.OK).json({jobs});
-});
-
-// POST Create Job
-app.post("/api/v1/jobs", (req, res) => {
-
-    const {company, position} = req.body;
-
-    if (!company || !position) {
-        return res.status(StatusCodes.BAD_REQUEST).json({msg: "Invalid request"});
-    }
-
-    const id = nanoid();
-    const job = {id, company, position};
-
-    jobs.push(job);
-
-    res.status(StatusCodes.CREATED).json({job});
-});
-
-app.get("/api/v1/jobs/:id", (req, res) => {
-    const {id} = req.params;
-
-    if (!id) {
-        return res.status(StatusCodes.BAD_REQUEST).json({msg: "Invalid request"});
-    }
-
-    const foundJob = jobs.find(job => job.id === id);
-
-    if(foundJob) {
-        return res.status(StatusCodes.OK).json(jobs.find(job => job.id === id));
-    }
-
-    return res.status(StatusCodes.NOT_FOUND).json({msg: "Job not found"});
-});
+export default app;
