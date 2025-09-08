510409e6445038a408a1fa4c7ea4d02c78f97457
Thales Raymond <32554150+thalesraymond@users.noreply.github.com>
2025-09-08 19:46:15 -0300

    Feature/error-routes (#33)
    * routing errors
    
    * error routing

--- src/server.ts
+++ src/server.ts
@@ -1,33 +1,39 @@
 import express from "express";
-import { Request, Response } from 'express';
 import morgan from "morgan";
 import * as dotenv from "dotenv";
 import jobRoutes from "./JobRoutes";
-
+import { StatusCodes } from "http-status-codes";
 
 const app = express();
 
 dotenv.config();
 
 if (process.env.NODE_ENV === "development") {
-    app.use(morgan("dev"));
+  app.use(morgan("dev"));
 }
 
 app.use(express.json());
 
 app.use(jobRoutes);
 
-app.get("/", (req: Request, res: Response) => {
-    res.send({msg: "Hello World!!"});
+
+
+app.use((_req, res) => {
+  return res.status(StatusCodes.NOT_FOUND).json({ msg: "Not Found" });
+});
+
+
+app.use((err: any, _req:any, res:any) => {
+  console.log(err);
+
+  return res
+    .status(StatusCodes.INTERNAL_SERVER_ERROR)
+    .json({ msg: "Something went wrong" });
 });
 
 app.listen(process.env.PORT ?? 5100, () => {
-    console.log("server running....");
+  console.log("server running....");
 });
 
-app.post("/api/v1/test", (req: Request, res: Response) => {
-    const {name} = req.body;
-    res.json({msg: `hello ${name}!`});
-});
 
 export default app;
