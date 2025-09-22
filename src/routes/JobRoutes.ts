49388d84512483066187d43bf47bf96a78a55e6a
Thales Raymond <32554150+thalesraymond@users.noreply.github.com>
2025-09-22 17:29:21 -0300

    express validator

--- src/routes/JobRoutes.ts
+++ src/routes/JobRoutes.ts
@@ -1,5 +1,5 @@
 import express from "express";
-import JobController from "../controllers/JobController.js";
+import JobController, { validateJobInput } from "../controllers/JobController.js";
 
 const jobRoutes = express.Router();
 
@@ -8,12 +8,12 @@ const jobController = new JobController();
 jobRoutes
   .route("/")
   .get(jobController.getAllJobs)
-  .post(jobController.createJob);
+  .post(validateJobInput, jobController.createJob);
 
 jobRoutes
   .route("/:id")
   .get(jobController.getJobById)
-  .put(jobController.updateJob)
+  .put(validateJobInput,jobController.updateJob)
   .delete(jobController.deleteJob);
 
 export default jobRoutes;
