cfbfac8f4b4d8cb1d65ee622839c5533baf00024
Thales Raymond <32554150+thalesraymond@users.noreply.github.com>
2025-10-07 15:53:39 -0300

    expiration in one day

--- src/utils/TokenUtils.ts
+++ src/utils/TokenUtils.ts
@@ -2,11 +2,13 @@ import jwt from "jsonwebtoken";
 import UnauthenticatedError from "../errors/UnauthenticatedError.js";
 
 export default class TokenUtils {
+    private static ONE_DAY = 86400;
+
     public static generateToken(payload: { userId: string; role: string }) {
         const secret = process.env.JWT_SECRET || "default_secret";
-        const expiresIn = process.env.JWT_EXPIRES_IN ?? "1d";
+        const expiresIn = (process.env.JWT_EXPIRES_IN ?? TokenUtils.ONE_DAY) as number;
 
-        return jwt.sign(payload, secret, { expiresIn });
+        return jwt.sign(payload, secret, { expiresIn: expiresIn});
     }
 
     public static verifyToken(token: string) {
