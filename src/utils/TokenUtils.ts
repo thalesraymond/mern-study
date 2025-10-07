8e0dfea9939ff9ec6d9eef45d5a512ab8e534506
Thales Raymond <32554150+thalesraymond@users.noreply.github.com>
2025-10-07 17:48:24 -0300

    fix lint errors

--- src/utils/TokenUtils.ts
+++ src/utils/TokenUtils.ts
@@ -1,23 +1,21 @@
 import jwt from "jsonwebtoken";
-import UnauthenticatedError from "../errors/UnauthenticatedError.js";
-import cookieParser from "cookie-parser";
 
 export default class TokenUtils {
     private static ONE_DAY = 86400;
 
     public static generateToken(payload: { userId: string; role: string }) {
         const secret = process.env.JWT_SECRET || "default_secret";
-        const expiresIn = (process.env.JWT_EXPIRES_IN ?? TokenUtils.ONE_DAY) as number;
+        const expiresIn = (process.env.JWT_EXPIRES_IN ??
+            TokenUtils.ONE_DAY) as number;
 
-        return jwt.sign(payload, secret, { expiresIn: expiresIn});
+        return jwt.sign(payload, secret, { expiresIn: expiresIn });
     }
 
-    public static verifyToken(token: string) : { userId: string; role: string } {
+    public static verifyToken(token: string): { userId: string; role: string } {
         const secret = process.env.JWT_SECRET || "default_secret";
-        
+
         const decoded = jwt.verify(token, secret);
-        
-        return decoded as { userId: string; role: string }
- 
+
+        return decoded as { userId: string; role: string };
     }
 }
