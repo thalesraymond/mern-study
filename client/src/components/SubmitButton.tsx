d5235c91ace36ed90f76987099dcbfb4ad920dcf
Thales Raymond <32554150+thalesraymond@users.noreply.github.com>
2025-10-15 20:57:32 -0300

    Refactor DashboardFormPage to TypeScript and create FormRowSelect component
    - Deleted the old DashboardFormPage.js and replaced it with DashboardFormPage.ts for TypeScript support.
    - Introduced a new FormRowSelect component for handling select inputs.
    - Updated SubmitButton component to accept additional CSS classes.
    - Modified AddJob page to utilize the new FormRowSelect and updated styling with the new Wrapper.
    - Added constants for job status and job type in a new Constants.ts file.

--- client/src/components/SubmitButton.tsx
+++ client/src/components/SubmitButton.tsx
@@ -1,17 +1,13 @@
 import { useNavigation } from "react-router-dom";
 
-const SubmitButton = (options: { text:string, submittingText: string }) => {
+const SubmitButton = (options: { text: string; submittingText: string; aditionalCssClasses?: string }) => {
     const navigation = useNavigation();
 
     const isSubmitting = navigation.state === "submitting";
 
     return (
-        <button
-            type="submit"
-            className="btn btn-block"
-            disabled={isSubmitting}
-        >
-            {isSubmitting ? options.submittingText : options.text }
+        <button type="submit" className={`btn btn-block ${options.aditionalCssClasses || ""}`} disabled={isSubmitting}>
+            {isSubmitting ? options.submittingText : options.text}
         </button>
     );
 };
--- client/src/pages/AddJob.tsx
+++ client/src/pages/AddJob.tsx
@@ -1,11 +1,39 @@
-
+import { FormRow, SubmitButton } from "../components";
+import Wrapper from "../assets/wrappers/DashboardFormPage";
+import { useOutletContext } from "react-router-dom";
+import { JOB_STATUS, JOB_TYPE } from "../utils/Constants";
+import { Form, useNavigation, redirect } from "react-router-dom";
+import { toast } from "react-toastify";
+import apiClient from "../utils/ApiClient";
+import FormRowSelect from "../components/FormRowSelect";
 
 const AddJob = () => {
-    
+    const { user } = useOutletContext() as { user: { location: string } };
+
     return (
-        <div>
-            <h1>AddJob</h1>
-        </div>
+        <Wrapper>
+            <Form method="post" className="form">
+                <h4 className="form-title">add job</h4>
+                <div className="form-center">
+                    <FormRow type="text" name="position" />
+                    <FormRow type="text" name="company" />
+                    <FormRow type="text" labelText="job location" name="jobLocation" defaultValue={user.location} />
+                    <FormRowSelect
+                        labelText="job status"
+                        name="jobStatus"
+                        defaultValue={JOB_STATUS.PENDING}
+                        list={Object.values(JOB_STATUS)}
+                    />
+                    <FormRowSelect
+                        name="jobType"
+                        labelText="job type"
+                        defaultValue={JOB_TYPE.FULL_TIME}
+                        list={Object.values(JOB_TYPE)}
+                    />
+                    <SubmitButton text="submit" submittingText="submitting..." aditionalCssClasses="form-btn" />
+                </div>
+            </Form>
+        </Wrapper>
     );
 };
 
