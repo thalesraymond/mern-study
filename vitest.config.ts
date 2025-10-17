db95b282e47b7f23ce6b2007976c3f2653dc5f6c
Thales Raymond <32554150+thalesraymond@users.noreply.github.com>
2025-10-16 21:24:34 -0300

    improve coverage ignores

--- vitest.config.ts
+++ vitest.config.ts
@@ -1,19 +1,25 @@
-
-import { defineConfig } from 'vitest/config';
+import { defineConfig } from "vitest/config";
 
 export default defineConfig({
-  test: {
-    coverage: {
-      include: ['src/**/*.ts'],
-      exclude: ['client/**', 'node_modules/**'],
-      provider: 'v8',
-      reporter: ['text', 'json', 'html'],
-      thresholds: {
-        lines: 0,
-        functions: 0,
-        branches: 0,
-        statements: 0,
-      }
+    test: {
+        coverage: {
+            include: ["src/**/*.ts"],
+            exclude: [
+                "client/**",
+                "node_modules/**",
+                "dist/**",
+                "src/middleware/**",
+                "src/routes/**",
+                "src/requests/**",
+            ],
+            provider: "v8",
+            reporter: ["text", "json", "html"],
+            thresholds: {
+                lines: 0,
+                functions: 0,
+                branches: 0,
+                statements: 0,
+            },
+        },
     },
-  },
 });
