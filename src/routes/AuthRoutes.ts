306ebed482b99e2edd2eb099987b6dcd3670916a
Thales Raymond <32554150+thalesraymond@users.noreply.github.com>
2025-10-17 00:04:49 -0300

    docker compose (#113)

--- src/routes/AuthRoutes.ts
+++ src/routes/AuthRoutes.ts
@@ -4,18 +4,13 @@ import UserValidator from "../validators/UserValidator.js";
 import { IUserRepository } from "../domain/repositories/IUserRepository.js";
 import { IJobRepository } from "../domain/repositories/IJobRepository.js";
 
-export default (
-    userRepository: IUserRepository,
-    jobRepository: IJobRepository
-) => {
+export default (userRepository: IUserRepository, jobRepository: IJobRepository) => {
     const authRoutes = express.Router();
     const userController = new UserController(userRepository, jobRepository);
 
-    authRoutes
-        .route("/login")
-        .post(UserValidator.loginUserValidation(), userController.auth);
+    authRoutes.route("/login").post(UserValidator.loginUserValidation(), userController.auth);
 
-    authRoutes.route("/logout").post(userController.logout);
+    authRoutes.route("/logout").get(userController.logout);
 
     return authRoutes;
 };
